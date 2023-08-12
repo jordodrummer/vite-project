c26991e4e27e69cd6341fe5344ba6bc5379f08c3
jordan <jordo.drummer@gmail.com>
2023-08-11 19:32:05 -0700

    commit4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
+import './App.css'
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
-
-  console.log("puppyList: ", puppyList);
-
+  const [featPupId, setFeatPupId] = useState(null)
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+      {puppies.map((puppy) => {
+             return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
            })
        }
+       
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
