eee9a27be2fe19eb336a8e4ab6c10bc22b6c6234
jordan <jordo.drummer@gmail.com>
2023-08-11 18:02:33 -0700

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
 
-  return (
-      <div>
+  const [puppies, setPuppies] = useState(puppyList);
 
-      </div>
-  )
+  console.log("puppyList: ", puppyList);
+
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
